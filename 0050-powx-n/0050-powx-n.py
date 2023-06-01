46f7bc5109b5569f446bf3a7a8390a19cbb649ac
Piyush Rai <111386563+piyushrai007@users.noreply.github.com>
2023-06-02 01:36:54 +0530

    Time: 15 ms (80.09%), Space: 13.3 MB (52.91%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        a = x ** n    
+                
+        return a    
