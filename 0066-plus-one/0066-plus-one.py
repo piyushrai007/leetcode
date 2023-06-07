c3d1438ea3eff3d35ddee329ac3c02c1068fa387
Piyush Rai <111386563+piyushrai007@users.noreply.github.com>
2023-06-07 22:38:30 +0530

    Time: 8 ms (99.45%), Space: 13.4 MB (21.96%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        digits[-1] += 1
+        for i in range(len(digits)-1, 0, -1):
+            if digits[i] != 10:
+                break
+            digits[i] = 0
+            digits[i-1] += 1
+    
+        if digits[0] == 10:
+            digits[0] = 0
+            return [1] + digits
+        return digits
+                
+
+                    
+                
