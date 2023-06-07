f95c4b6191c127c1d5139e7f426596427adfbdac
Piyush Rai <111386563+piyushrai007@users.noreply.github.com>
2023-06-07 23:22:14 +0530

    Added solution - LeetHub

---
+++ Least Prime Factor - GFG/least-prime-factor.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+class Solution:
+    def leastPrimeFactor (self, n):
+        # code here 
+        lpf = [0] * (n + 1)
+        lpf[1] = 1
+    
+        for i in range(2, n + 1):
+            if lpf[i] == 0:
+                lpf[i] = i
+                for j in range(i * i, n + 1, i):
+                    if lpf[j] == 0:
+                        lpf[j] = i
+    
+        return lpf
+       
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		ob = Solution();
+		ans = ob.leastPrimeFactor(n)
+		for i in range(1, n+1):
+			print(ans[i], end = " ")
+		print()
+
+# } Driver Code Ends
