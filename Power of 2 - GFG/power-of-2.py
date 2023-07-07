3853dd9054c8b2883ab4313e7d50a56a8884aa56
Piyush Rai <111386563+piyushrai007@users.noreply.github.com>
2023-07-07 23:56:38 +0530

    Added solution - LeetHub

---
+++ Power of 2 - GFG/power-of-2.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    # Function to check if given number n is a power of two.
+    def isPowerofTwo(self,n):
+        ##Your code here
+        if n<=0 :
+            return False
+            
+        elif n&(n-1)==0 :
+            return True
+        else:
+            return False
+        
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        ob=Solution()
+        if ob.isPowerofTwo(n):
+            print("YES")
+        else:
+            print("NO")
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
