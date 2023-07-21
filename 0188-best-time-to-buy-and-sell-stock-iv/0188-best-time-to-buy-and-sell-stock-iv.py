8af38b0e3bb04cbf8486c30867ecea8c78ec7872
Piyush Rai <111386563+piyushrai007@users.noreply.github.com>
2023-07-21 23:47:26 +0530

    Time: 342 ms (17.65%), Space: 18.3 MB (28.43%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,22 @@
+def memo(a,ind,buy,cap,dp):
+    if cap == 0:
+        return 0
+    if ind == len(a):
+        return 0
+    if dp[ind][buy][cap] != -1:
+        return dp[ind][buy][cap]
+    if buy:
+        dp[ind][buy][cap] = max(memo(a,ind+1,0,cap,dp)-a[ind],memo(a,ind+1,1,cap,dp))
+    else:
+        dp[ind][buy][cap]= max(memo(a,ind+1,1,cap-1,dp)+a[ind],memo(a,ind+1,0,cap,dp))
+    return dp[ind][buy][cap]
+class Solution(object):
+    def maxProfit(self, k, prices):
+        """
+        :type k: int
+        :type prices: List[int]
+        :rtype: int
+        """
+        t = [[[-1 for _ in range(k+1)] for _ in range(2)] for _ in range(len(prices))]
+
+        return memo(prices,0,1,k,t)
