a7b6fc3786fc70a7c5a0afafb91d247455a440fb
Piyush Rai <111386563+piyushrai007@users.noreply.github.com>
2023-07-25 15:40:58 +0530

    Time: 1660 ms (24.86%), Space: 52.4 MB (17.43%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,18 +1,23 @@
 class Solution(object):
-    def maxProfit(self,a):
+    def maxProfit(self,prices):
         """
         :type prices: List[int]
         :rtype: int
         """
-        n=len(a)
-        after = [[0 for _ in range(3)] for _ in range(2)]
-        cur = [[0 for _ in range(3)] for _ in range(2)]
-        for ind in range(n-1,-1,-1):
-            for buy in range(2):
-                for cap in range (1,3):
-                    if buy:
-                        cur[buy][cap] = max(after[0][cap]-a[ind],after[1][cap])
-                    else:
-                        cur[buy][cap] = max(after[1][cap-1]+a[ind],after[0][cap])
-            after = cur
-        return after[1][2] 
+        if len(prices) <= 1: return 0
+        n, k = len(prices), 2
+
+        B = [prices[i+1] - prices[i] for i in range(len(prices) - 1)]
+        if k > len(prices)//2: return sum(x for x in B if x > 0)
+        
+        dp = [[0]*(k+1) for _ in range(n-1)] 
+        mp = [[0]*(k+1) for _ in range(n-1)] 
+
+        dp[0][1], mp[0][1] = B[0], B[0]
+
+        for i in range(1, n-1):
+            for j in range(1, k+1):
+                dp[i][j] = max(mp[i-1][j-1], dp[i-1][j]) + B[i]
+                mp[i][j] = max(dp[i][j], mp[i-1][j])
+
+        return max(mp[-1])
