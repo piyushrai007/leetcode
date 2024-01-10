3dba1340d2998fe8e201d7171a50e82f1f1d30bc
Piyush Rai <111386563+piyushrai007@users.noreply.github.com>
2024-01-10 22:12:27 +0530

    Time: 42 ms (47.02%), Space: 14.3 MB (16.14%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,23 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        test = ""
-        # Result
-        maxLength = -1
-     
-        # Return zero if string is empty
-        if (len(s) == 0):
-            return 0
-        elif(len(s) == 1):
-            return 1
-        for c in list(s):
-            current = "".join(c)
-         
-        # If string already contains the character
-        # Then substring after repeating character
-            if (current in test):
-                test = test[test.index(current) + 1:]
-            test = test + "".join(c)
-            maxLength = max(len(test), maxLength)
-        return maxLength        
+        dict = {s[i]:-1 for i in range(0,len(s))}
+
+        maxline = 0
+        start =-1
+        for i in range(0,len(s)):
+            if dict[s[i]]>start:
+                start = dict[s[i]]
+            dict[s[i]] = i
+            maxline = max(maxline,i-start)
+        return maxline
         
