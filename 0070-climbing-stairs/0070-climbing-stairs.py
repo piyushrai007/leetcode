33a3ad66770a4fe62fffda4fa5f7e6752b0d9db6
Piyush Rai <111386563+piyushrai007@users.noreply.github.com>
2024-01-18 18:33:48 +0000

    Time: 7 ms (95.56%), Space: 13.2 MB (88.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,12 @@
-def climb(n):
-    if n==1: #only one step option is availble
-        return 1
-    if n ==2: # two options are possible : to take two 1-stpes or to only take one 2-steps
-        return 2
-    return climb(n-1) + climb(n-2)
+import math
 class Solution(object):
     def climbStairs(self, n):
         """
         :type n: int
         :rtype: int
         """
-        a = b = 1
-        for _ in range(n):
-            a, b = b, a + b
-        return a
+        dp=[0]*(n+1)
+        dp[0]=1
+        for i in range(1,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
