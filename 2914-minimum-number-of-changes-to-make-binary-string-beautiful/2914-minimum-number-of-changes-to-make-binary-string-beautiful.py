e692bb493c555cd0f61d0cf3b416f89492f13093
Piyush Rai <111386563+piyushrai007@users.noreply.github.com>
2024-11-05 23:05:53 +0530

    Time: 21 ms (55.41%), Space: 16.8 MB (89.02%) - LeetHub

---
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -0,0 +1,5 @@
+class Solution:
+    def minChanges(self, s: str) -> int:
+        # Count changes needed for each unmatched pair
+        return sum(s[i] != s[i + 1] for i in range(0, len(s), 2))
+    
